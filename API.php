ba83fa05605762d2fdf6a638b9295bd57807a463
GabHurlant <gabin.lasne@gmail.com>
2024-03-25 10:13:13 +0100

    Ajouter le fichier .htaccess et l'API pour les magasins

---
+++ API.php
@@ -0,0 +1,40 @@
+<?php
+
+//API pour les magasins
+
+//Headers requis
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE");
+header("Access-Control-Allow-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//Inclusion des fichiers de configuration et d'accès aux données
+require_once 'bootstrap.php';
+
+use Entity\Stores;
+use Entity\Products;
+use Entity\Categories;
+use Entity\Brands;
+use Entity\Employees;
+use Entity\Stocks;
+
+//Récupération de la méthode HTTP
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET':
+        //Code pour la méthode GET
+        if (isset($_GET['products'])) {
+            $data = array();
+            $stores = $entityManager->getRepository(Products::class)->findAll();
+            foreach ($profucts as $product) {
+                $data[] = $product->toArray();
+            }
+            echo json_encode($data);
+        } else {
+            $response = array("status" => 0, "message" => "Invalid Request");
+        }
+        break;
+}
