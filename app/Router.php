844f269c015623f8d1c0d01de8c449c926fd74f1
GabHurlant <gabin.lasne@gmail.com>
2024-04-18 18:56:58 +0200

    Mise à jour de la classe Router pour ajouter des commentaires et des annotations de méthode.

--- app/Router.php
+++ app/Router.php
@@ -1,17 +1,35 @@
 <?php
 
-//router
-
+/**
+ * Class Router
+ *
+ * A simple router class for handling HTTP requests.
+ */
 class Router
 {
-
+    /**
+     * @var array $routes The array to store routes.
+     */
     private $routes = [];
 
+    /**
+     * Adds a route to the routes array.
+     *
+     * @param string $method The HTTP method for the route.
+     * @param string $url The URL pattern for the route.
+     * @param callable $handler The handler to be called when the route is matched.
+     */
     public function addRoute($method, $url, $handler)
     {
         $this->routes[] = [$method, $url, $handler];
     }
 
+    /**
+     * Dispatches the router to handle the current request.
+     *
+     * @param string $method The HTTP method of the current request.
+     * @param string $url The URL of the current request.
+     */
     public function dispatch($method, $url)
     {
         foreach ($this->routes as $route) {
@@ -23,7 +41,7 @@ class Router
                 }
             }
         }
-        //if no route
+        // If no route is found, return a 404 response.
         header('HTTP/1.0 404 Not Found');
         echo '404 - Not Found';
     }
--- index.php
+++ index.php
@@ -1,19 +1,58 @@
 <?php
+
+/**
+ * Allows cross-origin requests from all origins.
+ */
 header("Access-Control-Allow-Origin: *");
+
+/**
+ * Allows the following HTTP methods: GET, POST, PUT, DELETE.
+ */
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+/**
+ * Allows the Content-Type header in HTTP requests.
+ */
 header("Access-Control-Allow-Headers: Content-Type");
 
+/**
+ * Includes the bootstrap file from the app directory.
+ */
 require_once __DIR__ . "/app/bootstrap.php";
+
+/**
+ * Includes the Router file from the app directory.
+ */
 require __DIR__ . "/app/Router.php";
+
+/**
+ * Includes the routes configuration file from the app directory.
+ */
 $routes = require __DIR__ . "/app/config/routes.php";
 
+/**
+ * Creates a new instance of the Router class.
+ */
 $router = new Router();
 
+/**
+ * Gets the HTTP method of the current request.
+ */
 $method = $_SERVER['REQUEST_METHOD'];
+
+/**
+ * Gets the URI of the current request.
+ */
 $url = $_SERVER['REQUEST_URI'];
 
+/**
+ * Adds each route from the routes configuration to the router.
+ */
 foreach ($routes as $route) {
     $router->addRoute($route['method'], $route['path'], [$route['controller'], $route['action']]);
 }
 
+/**
+ * Dispatches the router to handle the current request.
+ */
 $router->dispatch($method, $url);
