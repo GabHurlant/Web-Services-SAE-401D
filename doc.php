80b471b8ed38fd9bdc0d7f5e6b31f556ddf01599
GabHurlant <gabin.lasne@gmail.com>
2024-04-19 10:04:19 +0200

    cleaning swagger ui

--- doc.php
+++ doc.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Swagger UI Example</title>
+    <title>API Documentation</title>
 
     <!-- Import Swagger UI from CDN -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.16.0/swagger-ui.css">
